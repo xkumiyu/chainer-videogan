32d9cc8b5f4721853ee1b0adf3aa55633705a37d
xkumiyu <xkumiyu@gmail.com>
2017-09-12 02:05:10 +0900

    Use initializers

--- net.py
+++ net.py
@@ -4,79 +4,52 @@ import chainer.functions as F
 import chainer.links as L
 
 
-# TODO: 平均0, 標準偏差0.01のガウスノイズですべての重みを初期化
-class Block2D(chainer.Chain):
-    def __init__(self, out_channels, ksize=4, stride=2, pad=1):
-        super(Block2D, self).__init__()
-        with self.init_scope():
-            self.dc = L.Deconvolution2D(None, out_channels, ksize, stride, pad)
-            self.bn = L.BatchNormalization(out_channels)
-
-    def __call__(self, x):
-        h = self.dc(x)
-        h = self.bn(h)
-        return F.relu(h)
-
-
-class Block3D(chainer.Chain):
-    def __init__(self, in_channels, out_channels, ksize=4, stride=2, pad=1):
-        super(Block3D, self).__init__()
-        with self.init_scope():
-            self.dc = L.DeconvolutionND(
-                3, in_channels, out_channels, ksize, stride, pad)
-            self.bn = L.BatchNormalization(out_channels)
-
-    def __call__(self, x):
-        h = self.dc(x)
-        h = self.bn(h)
-        return F.relu(h)
-
-
 class Generator(chainer.Chain):
-    def __init__(self, noise_dim=100, bottom_time=2, bottom_width=4, ch=512):
+    def __init__(self):
         super(Generator, self).__init__()
-        self.noise_dim = noise_dim
-        self.ch = ch
-        self.bottom_time = bottom_time
-        self.bottom_width = bottom_width
 
         with self.init_scope():
-            self.fg_l0 = L.Linear(
-                self.noise_dim, self.bottom_time * self.bottom_width * self.bottom_width * self.ch)
-            self.fg_bn0 = L.BatchNormalization(
-                self.bottom_time * self.bottom_width * self.bottom_width * self.ch)
-            # self.fg_block1 = Block3D(ch, ch // 2, ksize=(2, 4, 4))
-            self.fg_block1 = Block3D(ch, ch // 2, ksize=(4, 4, 4))
-            self.fg_block2 = Block3D(ch // 2, ch // 4)
-            self.fg_block3 = Block3D(ch // 4, ch // 8)
-            self.fg_dc4 = L.DeconvolutionND(
-                3, ch // 8, 3, ksize=4, stride=2, pad=1)
-            self.m_dc4 = L.DeconvolutionND(
-                3, ch // 8, 1, ksize=4, stride=2, pad=1)
+            w = chainer.initializers.Normal(0.01)
 
-            self.bg_l0 = L.Linear(
-                self.noise_dim, self.bottom_width * self.bottom_width * self.ch)
-            self.bg_bn0 = L.BatchNormalization(
-                self.bottom_width * self.bottom_width * self.ch)
-            self.bg_block1 = Block2D(ch // 2)
-            self.bg_block2 = Block2D(ch // 4)
-            self.bg_block3 = Block2D(ch // 8)
-            self.bg_dc4 = L.Deconvolution2D(
-                ch // 8, 3, ksize=4, stride=2, pad=1)
+            self.fg_l0 = L.Linear(None, 2 * 4 * 4 * 512, initialW=w)
+            self.fg_dc0 = L.DeconvolutionND(3, 100, 512, (2, 4, 4), initialW=w)
+            self.fg_dc1 = L.DeconvolutionND(3, 512, 256, 4, 2, 1, initialW=w)
+            self.fg_dc2 = L.DeconvolutionND(3, 256, 128, 4, 2, 1, initialW=w)
+            self.fg_dc3 = L.DeconvolutionND(3, 128, 64, 4, 2, 1, initialW=w)
+            self.fg_dc4 = L.DeconvolutionND(3, 64, 3, 4, 2, 1, initialW=w)
+            self.m_dc4 = L.DeconvolutionND(3, 64, 1, 4, 2, 1, initialW=w)
+            self.fg_bn0 = L.BatchNormalization(512)
+            self.fg_bn1 = L.BatchNormalization(256)
+            self.fg_bn2 = L.BatchNormalization(128)
+            self.fg_bn3 = L.BatchNormalization(64)
+
+            self.bg_l0 = L.Linear(None, 4 * 4 * 512, initialW=w)
+            self.bg_dc0 = L.Deconvolution2D(100, 512, 4, initialW=w)
+            self.bg_dc1 = L.Deconvolution2D(512, 256, 4, 2, 1, initialW=w)
+            self.bg_dc2 = L.Deconvolution2D(256, 128, 4, 2, 1, initialW=w)
+            self.bg_dc3 = L.Deconvolution2D(128, 64, 4, 2, 1, initialW=w)
+            self.bg_dc4 = L.Deconvolution2D(64, 3, 4, 2, 1, initialW=w)
+            self.bg_bn0 = L.BatchNormalization(512)
+            self.bg_bn1 = L.BatchNormalization(256)
+            self.bg_bn2 = L.BatchNormalization(128)
+            self.bg_bn3 = L.BatchNormalization(64)
 
     def make_noize(self, batchsize):
         return np.random.randn(batchsize * self.noise_dim)\
             .reshape(batchsize, self.noise_dim).astype(np.float32)
 
     def foreground(self, z):
+        """
+        input z shape:     (batchsize, 100)
+        output fg shape:   (batchsize, 3, 32, 64, 64)
+        output mask shape: (batchsize, 1, 32, 64, 64)
+        """
         batchsize = len(z)
-
-        h = F.reshape(
-            F.relu(self.fg_bn0(self.fg_l0(z))),
-            (batchsize, self.ch, self.bottom_time, self.bottom_width, self.bottom_width))
-        h = self.fg_block1(h)
-        h = self.fg_block2(h)
-        h = self.fg_block3(h)
+        z = z.reshape(batchsize, 100, 1, 1, 1)
+        h = F.relu(self.fg_bn0(self.fg_dc0(z)))
+        h = F.relu(self.fg_bn1(self.fg_dc1(h)))
+        h = F.relu(self.fg_bn2(self.fg_dc2(h)))
+        h = F.relu(self.fg_bn3(self.fg_dc3(h)))
         # TODO: シグモイド関数をかけたあとにスパースさせる
         # we also add to the objective a small sparsity prior on the mask λ||m(z)|| for λ = 0.1
         mask = F.sigmoid(self.m_dc4(h))
@@ -84,23 +57,30 @@ class Generator(chainer.Chain):
         return fg, mask
 
     def background(self, z):
+        """
+        input z shape:  (batchsize, 100)
+        output h shape: (batchsize, 3, 64, 64)
+        """
         batchsize = len(z)
-
-        h = F.reshape(F.relu(self.bg_bn0(self.bg_l0(z))),
-                      (batchsize, self.ch, self.bottom_width, self.bottom_width))
-        h = self.bg_block1(h)
-        h = self.bg_block2(h)
-        h = self.bg_block3(h)
+        z = z.reshape(batchsize, 100, 1, 1)
+        h = F.relu(self.bg_bn0(self.bg_dc0(z)))
+        h = F.relu(self.bg_bn1(self.bg_dc1(h)))
+        h = F.relu(self.bg_bn2(self.bg_dc2(h)))
+        h = F.relu(self.bg_bn3(self.bg_dc3(h)))
         return F.tanh(self.bg_dc4(h))
 
     def __call__(self, z):
+        """
+        input z shape:  (batchsize, 100) or (batchsize, 1024, 4, 4)
+        output x shape: (batchsize, 3, 32, 64, 64)
+        """
+        # TODO: (batchsize, 1024, 4, 4)に対応
         batchsize = len(z)
 
         fg, mask = self.foreground(z)
         mask = F.tile(mask, (1, 3, 1, 1, 1))
 
         bg = self.background(z)
-        # TODO: ハードコーディングをやめる
         bg = F.reshape(bg, (batchsize, 3, 1, 64, 64))
         bg = F.tile(bg, (1, 1, 32, 1, 1))
 
@@ -109,27 +89,30 @@ class Generator(chainer.Chain):
 
 
 class Discriminator(chainer.Chain):
-    def __init__(self, bottom_time=2, bottom_width=4, ch=512):
+    def __init__(self):
         super(Discriminator, self).__init__()
+
         with self.init_scope():
-            self.c0 = L.ConvolutionND(3, 3, ch // 8, ksize=4, stride=2, pad=1)
-            self.c1 = L.ConvolutionND(
-                3, ch // 8, ch // 4, ksize=4, stride=2, pad=1)
-            self.c2 = L.ConvolutionND(
-                3, ch // 4, ch // 2, ksize=4, stride=2, pad=1)
-            self.c3 = L.ConvolutionND(3, ch // 2, ch, ksize=4, stride=2, pad=1)
-            self.c4 = L.ConvolutionND(
-                3, ch, ch, ksize=(2, 4, 4), stride=2, pad=0)
-            self.l5 = L.Linear(ch, 1)
-            self.bn1 = L.BatchNormalization(ch // 4)
-            self.bn2 = L.BatchNormalization(ch // 2)
-            self.bn3 = L.BatchNormalization(ch)
-            self.bn4 = L.BatchNormalization(ch)
+            w = chainer.initializers.Normal(0.01)
+            self.l5 = L.Linear(512, 1, initialW=w)
+            self.conv0 = L.ConvolutionND(3, 3, 64, 4, 2, 1, initialW=w)
+            self.conv1 = L.ConvolutionND(3, 64, 128, 4, 2, 1, initialW=w)
+            self.conv2 = L.ConvolutionND(3, 128, 256, 4, 2, 1, initialW=w)
+            self.conv3 = L.ConvolutionND(3, 256, 512, 4, 2, 1, initialW=w)
+            self.conv4 = L.ConvolutionND(3, 512, 512, (2, 4, 4), initialW=w)
+            self.bn1 = L.BatchNormalization(128)
+            self.bn2 = L.BatchNormalization(256)
+            self.bn3 = L.BatchNormalization(512)
+            self.bn4 = L.BatchNormalization(512)
 
     def __call__(self, x):
-        h = F.leaky_relu(self.c0(x))
-        h = F.leaky_relu(self.bn1(self.c1(h)))
-        h = F.leaky_relu(self.bn2(self.c2(h)))
-        h = F.leaky_relu(self.bn3(self.c3(h)))
-        h = F.leaky_relu(self.bn4(self.c4(h)))
+        """
+        input x shape:  (batchsize, 3, 32, 64, 64)
+        output y shape: (batchsize, 1)
+        """
+        h = F.leaky_relu(self.conv0(x))
+        h = F.leaky_relu(self.bn1(self.conv1(h)))
+        h = F.leaky_relu(self.bn2(self.conv2(h)))
+        h = F.leaky_relu(self.bn3(self.conv3(h)))
+        h = F.leaky_relu(self.bn4(self.conv4(h)))
         return self.l5(h)
